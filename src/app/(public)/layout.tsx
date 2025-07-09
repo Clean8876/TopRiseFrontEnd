30e18312725eb5b88363d1930656d56234513de4
chetanv18~ <chetan@gully2global.com>
2025-07-09 10:46:18 +0530

    layout setup

---
+++ src/app/(public)/layout.tsx
@@ -0,0 +1,12 @@
+import AdminLayoutWrapper from "../applayout/AdminLayoutWrapper";
+
+
+export default function PublicLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <AdminLayoutWrapper showHeaderFooter={true}>
+        {children}
+      </AdminLayoutWrapper>
+    </>
+  );
+} 
